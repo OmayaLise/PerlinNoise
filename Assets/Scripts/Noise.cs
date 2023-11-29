41f14d9ee6bd5b3237e986f24227208a5030e222
Bryan Bachelet <b.bachelet@student.isartdigital.com>
2023-11-29 09:48:57 +0100

    Add base Project

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public float[,] GenerateNoiseMap()
+    {
+        float[,] array = new float[0,0];
+        return array;
+    }
+}
