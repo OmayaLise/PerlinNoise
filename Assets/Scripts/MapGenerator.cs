5aeb3a92b8ab32021231e75c8b9516cc6c7392f8
Omaya LISE <omaya.lise@hotmail.fr>
2023-11-29 11:01:40 +0100

    Add material + map generator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    GameObject plane;
     private void Start()
     {
         CreateMap();
+        Create3DMap();
     }
     public void CreateMap()
     {
-        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+    }
+
+    public void Create3DMap()
+    {
+        //Vector3[] vertices = plane.GetComponent<MeshFilter>().mesh.vertices;
+
+        //for (int i = 0; i < Noise.GenerateNoiseMap(3, 3).Length; i++)
+        //{
+        //    for (int j = 0;j < Noise.GenerateNoiseMap(3, 3).Length; j++)
+        //        vertices[i].y= Noise.GenerateNoiseMap(3, 3)[i,j];
+        //}
+
+        //plane.GetComponent<MeshFilter>().mesh.vertices = vertices;
+        //plane.GetComponent<MeshFilter>().mesh.RecalculateNormals();
+
     }
 }
